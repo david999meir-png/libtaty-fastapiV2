941de4f62311d9d4c99f0c7092cb389b71e5a422
david999meir-png <david999meir@gmail.com>
2026-06-15 12:27:17 +0300

    add the members_db

---
+++ database/members_db.py
@@ -0,0 +1,89 @@
+from database.db_connection import get_connection
+
+class MemberDAL:
+    def create_member(data: dict):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """INSERT INTO members (name, email) VALUES (%s, %s)"""
+                cursor.execute(sql,list(data.values()))
+
+                conn.commit()
+                return cursor.lastrowid
+
+    def get_all_members():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql  = """SELECT * FROM members"""
+                cursor.execute(sql)
+
+                return cursor.fetchall()
+            
+    
+    def get_member_by_id(id):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql  = """SELECT * FROM members WHERE id = %s"""
+                cursor.execute(sql, (id,))
+
+                return cursor.fetchone()
+    
+    def update_member(id: int, data: dict):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                list_couse = [f"{key}=%s" for key in data.keys()]
+                txt_couse = ", ".join(list_couse)
+
+                sql = f"UPDATE members SET {txt_couse} WHERE id = %s"
+                values = list(data.values()) + [id]
+
+                cursor.execute(sql, values)
+                conn.commit()
+
+                return cursor.rowcount > 0
+    
+    def deactivate_member(id):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """UPDATE members SET is_active = FALSE WHERE id = %s"""
+                cursor.execute(sql, (id,))
+
+                conn.commit()
+                return cursor.rowcount > 0
+    
+    def activate_member(id):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """UPDATE members SET is_active = TRUE WHERE id = %s"""
+                cursor.execute(sql, (id,))
+
+                conn.commit()
+                return cursor.rowcount > 0
+    
+    def increment_borrows(id):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """UPDATE members SET total_borrows = total_borrows + 1 WHERE id = %s"""
+                cursor.execute(sql, (id,))
+                conn.commit()
+
+                return cursor.rowcount > 0
+    
+    def count_active_members():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT COUNT(*) FROM members WHERE is_active = TRUE"""
+                cursor.execute(sql)
+
+                return cursor.fetchone()
+            
+    def get_top_member():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT * FROM members 
+                        ORDER BY total_borrows DEASC
+                        LINIT 1"""
+                cursor.execute(sql)
+
+                return cursor.fetchone()
+            
+            
