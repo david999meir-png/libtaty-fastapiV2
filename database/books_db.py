326b3d7320d305581d44d1955e073aebf7f75bb6
david999meir-png <david999meir@gmail.com>
2026-06-15 11:57:29 +0300

    add the book_db

---
+++ database/books_db.py
@@ -0,0 +1,96 @@
+import logging
+from database.db_connection import get_connection
+
+class BookDAL:
+    def create_book(data: dict):
+        with get_connection() as conn:
+            with conn.cursor() as cursor:
+
+                sql = """
+                    INSERT INTO books(title, author, genre)
+                      VALUES (%s, %s, %s)
+                """
+                cursor.execute(sql, list(data.values()))
+                conn.commit()
+                return cursor.lastrowid
+            
+    def get_all_bodks():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql ="SELECT * FROM books"
+                cursor.execute(sql)
+
+                return cursor.fetchall()
+    
+    def get_book_by_id(id: int):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = "SELECT * FROM books WHERE id = %s"
+                cursor.execute(sql, (id,))
+
+                return cursor.fetchone()
+        
+    def update_book(id: int, data: dict):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                list_cause = [f"{key}=%s" for key in data]
+                txt_cause = ", ".join(list_cause)
+
+                sql = f"UPDATE books SET {txt_cause} WHERE id = %s"
+                vaues = list(data.values()) + [id]
+
+                cursor.execute(sql, vaues)
+                conn.commit()
+
+                return cursor.rowcount > 0
+        
+    def set_available(id: int, val: bool, member_id: int):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """UPDATE books SET is_available = %s ,borrowed_by_member_id = %s WHERE id = %s"""
+                cursor.execute(sql, (val, member_id, id))
+
+                conn.commit()
+                return cursor.rowcount > 0
+    
+    def count_total_books():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT COUNT(*) AS total_books FROM books"""
+                cursor.execute(sql)
+
+                return cursor.fetchone()
+    
+    def count_available_books():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT COUNT(is_available) AS total_available WHERE is_available = TRUE"""
+                cursor.execute(sql)
+
+                return cursor.fetchone()
+            
+    def count_borrowed_books():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT COUNT(is_available) AS total_available WHERE is_available = FALSE"""
+                cursor.execute(sql)
+
+                return cursor.fetchone()
+            
+    def count_by_genre():
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT genre, COUNT(*) AS total FROM books GROUP BY genre"""
+                cursor.execute(sql)
+
+                return cursor.fetchall()
+            
+    def count_active_borrows_by_member(member_id):
+        with get_connection() as conn:
+            with conn.cursor(dictionary=True) as cursor:
+                sql = """SELECT COUNT(*) AS total_member_books
+                        FROM books WHERE borrowed_by_member_id = %s"""
+                
+                cursor.execute(sql)
+                return cursor.fetchone()
+            
